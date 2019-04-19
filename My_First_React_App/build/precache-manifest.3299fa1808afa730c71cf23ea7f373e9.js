63243ea6e4a1303a40b3b492a77ecd05cbb05542
jitu <jitu@gmail.com>
2019-04-19 22:51:50 +0530

    commit first app

---
+++ My_First_React_App/build/precache-manifest.3299fa1808afa730c71cf23ea7f373e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11a9765c434382d49d69",
+    "url": "/static/css/main.e2f7efae.chunk.css"
+  },
+  {
+    "revision": "11a9765c434382d49d69",
+    "url": "/static/js/main.8f908298.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e4e41068823728a5c657393250fba9d",
+    "url": "/index.html"
+  }
+];
