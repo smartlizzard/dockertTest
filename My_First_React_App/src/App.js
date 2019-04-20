ddb640ad36d96825f3205dda2b86862d33c42b6c
jitu <jitu@gmail.com>
2019-04-20 11:00:48 +0530

    commit

--- My_First_React_App/src/App.js
+++ My_First_React_App/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Jitu !!!
+            Hi Jitu !!! How r u??
           </p>
           <a
             className="App-link"
